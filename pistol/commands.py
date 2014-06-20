c0cc924242ba31991a5d6ffbc472b36f4e885373
Dawson Reid <dreid93@gmail.com>
2014-06-20 00:15:02 -0300

    Git init now creates a package.yaml file. The package.yaml file format is not complete, though this is considered to close #1.

--- pistol/commands.py
+++ pistol/commands.py
@@ -4,22 +4,62 @@ Dawson Reid (dreid93@gmail.com)
 '''
 import logging
 
+def query_yes_no(question):
+    answer = raw_input('{0} (y/N) : '.format(question)).lower()
+    return answer[:1] == 'y'
+
+def query_string(question, default):
+    answer = raw_input(question)
+    return default if answer == '' else answer
+
+
 class init(object):
 
     def __init__(self):
         self.logger = logging.getLogger('{0}'.format(self))
 
     def __call__(self, args):
-        self.logger.info('Initializing the package.yaml.')
-        self.logger.debug(args)
+        self.logger.info('Initializing the package.yaml.\n')
 
-        import string
+        package = {
+            'name': query_string('name ({0}) : '.format(args.name), args.name),
+            'version': raw_input('version ({0}) : '.format(args.version)),
+            'description': raw_input('description ({0}) : '.format(args.description)),
+            'authors': [],
+            'keywords': [],
+            'bin': []
+            'dependencies': {
+                'common': []
+            }
+        }
 
-        tamplate = string.Template('''
-        
-        ''')
+        # add authors
+        add_author = query_yes_no('\nWould you like to add an author?')
+        while add_author:
+            author = {
+                'name': raw_input('name : '),
+                'email': raw_input('email : '),
+                'uri': raw_input('uri : ')
+            }
+            package['authors'].append(author)
+
+            add_author = query_yes_no('Would you like to add another author?')
+
+        # add keywords
+        add_keywords = query_yes_no('\nWould you like to add a keyword?')
+        while add_keywords:
+            keyword = raw_input('keyword : ')
+            package['keywords'].append(keyword)
+
+            add_keyword = query_yes_no('Would you like to add another keyword?')
 
 
+        self.logger.info('Done build the package.yaml.\n')
+
+        f = open('package.yaml', 'w')
+        import yaml
+        yaml.dump(package, f, default_flow_style=False)
+        f.close()
 
 
 class install(object):
